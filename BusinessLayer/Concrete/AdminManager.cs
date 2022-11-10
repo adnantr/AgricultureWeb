ab0e5286fb0a91a6b9e5a0cb300707b11cf5541c
Adnan Tatar <57808681+adnantr@users.noreply.github.com>
2022-11-10 15:01:50 +0300

    Add files via upload

---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class AdminManager : IAdminService
+    {
+		private readonly IAdminDal _adminDal;
+
+		public AdminManager(IAdminDal adminDal)
+		{
+			_adminDal = adminDal;
+		}
+
+		void IGenericService<Admin>.Delete(Admin t)
+		{
+			_adminDal.Delete(t);
+		}
+
+		Admin IGenericService<Admin>.GetById(int id)
+		{
+			var result=_adminDal.GetById(id);
+			return result;
+		}
+
+		List<Admin> IGenericService<Admin>.GetListAll()
+		{
+			return _adminDal.GetListAll();
+		}
+
+		void IGenericService<Admin>.Insert(Admin t)
+		{
+			_adminDal.Insert(t);
+		}
+
+		void IGenericService<Admin>.Update(Admin t)
+		{
+			_adminDal.Update(t);
+		}
+	}
+}
---
+++ BusinessLayer/ValidationRules/ImageValidation.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ImageValidation : AbstractValidator<Image>
+    {
+        public ImageValidation()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Görsel başlıpını boş geçemezsiniz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Görsel açıklamasını boş geçemezsiniz");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Görsel yolunu boş geçemezsiniz");
+            RuleFor(x => x.Title).MaximumLength(20).WithMessage("Lütfen en fazla 20 karater veri girişi yapınız.");
+        
+        }
+    }
+}
