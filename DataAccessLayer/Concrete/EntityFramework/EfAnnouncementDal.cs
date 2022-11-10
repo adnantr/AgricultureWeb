b6a4fc9dd219273c162cefe7a3bbc3f0dbdec908
Adnan Tatar <57808681+adnantr@users.noreply.github.com>
2022-11-10 15:03:10 +0300

    Add files via upload

---
+++ DataAccessLayer/Concrete/EntityFramework/EfAnnouncementDal.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repository;
+using DataAccessLayer.Contexts;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.EntityFramework
+{
+    public class EfAnnouncementDal : GenericRepository<Announcement>, IAnnouncementDal
+    {
+        public void AnnouncementStatusToFalse(int id)
+        {
+            using var context = new TarimContext();
+            Announcement p = context.Announcements.Find(id);
+            p.Status = false;
+            context.SaveChanges();
+        }
+
+        public void AnnouncementStatusToTrue(int id)
+        {
+            using var context = new TarimContext();
+            Announcement p = context.Announcements.Find(id);
+            p.Status = true;
+            context.SaveChanges();
+        }
+    }
+}
